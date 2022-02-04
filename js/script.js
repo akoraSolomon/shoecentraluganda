bf9a19f30cb8a2f05eb8da421790c8cacdcda376
Akora Solomon <35068415+akoraSolomon@users.noreply.github.com>
2022-02-04 18:40:53 +0300

    mobile media queries

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
-document.getElementById("ham_icon").onclick = function(){
-    this.style
+var input_field = document.getElementById("search_shoe");
+var search_form = document.getElementById("search_form");
+
+document.getElementById('search_icon').onclick = function(){
+    search_form.style.width = "90%";
+    search_form.style.margin = "auto";
+    search_form.style.display = "inline";
+    input_field.style.width = "100%";
+    
 }
