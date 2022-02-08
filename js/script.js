2014a2c32983e9eb62076215c04671f9934d5c60
Akora Solomon <35068415+akoraSolomon@users.noreply.github.com>
2022-02-08 14:41:11 +0300

    js file manipulation

--- js/script.js
+++ js/script.js
@@ -1,18 +1,17 @@
 var input_field = document.getElementById("search_shoe");
 var search_form = document.getElementById("search_form");
+var dropdown = document.getElementById("dropdown_id");
+let year = new Date();
 
 document.getElementById('search_icon').onclick = function(){
-    search_form.style.width = "90%";
+    search_form.style.width = "98%";
     search_form.style.margin = "auto";
-    search_form.style.display = "inline";
-    input_field.style.width = "100%";
+    search_form.style.display = "block";
     
 }
 
-let year = new Date();
 document.getElementById("date").innerHTML = year.getFullYear();
 
-var dropdown = document.getElementById("dropdown_id");
 document.getElementById("ham_icon").onclick = function(){
-    dropdown.style.display = "inline-block";
+    dropdown.style.display = "block";
 }
