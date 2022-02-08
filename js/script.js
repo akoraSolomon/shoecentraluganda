03e87c42370ba4cc5178af6a2c41a5c8ceea66a8
Akora Solomon <35068415+akoraSolomon@users.noreply.github.com>
2022-02-08 17:09:45 +0300

    input field all through shadows updated

--- js/script.js
+++ js/script.js
@@ -4,14 +4,15 @@ var dropdown = document.getElementById("dropdown_id");
 let year = new Date();
 
 document.getElementById('search_icon').onclick = function(){
-    search_form.style.width = "98%";
+    search_form.style.width = "90%";
     search_form.style.margin = "auto";
     search_form.style.display = "block";
+    dropdown.style.display = "none";
     
+}
+document.getElementById("ham_icon").onclick = function(){
+    search_form.style.display = "none";
+    dropdown.style.display = "block";
 }
 
 document.getElementById("date").innerHTML = year.getFullYear();
-
-document.getElementById("ham_icon").onclick = function(){
-    dropdown.style.display = "block";
-}
