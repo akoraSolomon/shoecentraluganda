7c71ef5541aa08ba6d34bc6ff0d8acfb7107d791
Akora Solomon <35068415+akoraSolomon@users.noreply.github.com>
2022-02-11 19:13:15 +0300

    shopping bag, search cancel

--- js/script.js
+++ js/script.js
@@ -1,18 +1,22 @@
 var input_field = document.getElementById("search_shoe");
 var search_form = document.getElementById("search_form");
 var dropdown = document.getElementById("dropdown_id");
+var search_icon = document.getElementById('search_icon');
+var cancel_search = document.getElementById('cancel_search');
 var img_container = document.getElementById("img_hero");
-var cancel = document.getElementById("cancel");
-var ham_icon = document.getElementById("ham_icon")
+var ham_icon = document.getElementById("ham_icon");
+var cancel_dropdown = document.getElementById("cancel_dropdown");
 const female_shoe_category = document.getElementsByClassName("female_shoes");
 
 let year = new Date();
-img_container.onscroll = function(){
+window.onscroll = function(){
     cancel.style.display = "none";
     dropdown.style.transform = "translateX(-150%)";
-    this.style.opacity = "1";
+    img_container.style.opacity = "1";
+    ham_icon.style.display = "inline";
+    
 }
-cancel.onclick = function(){
+cancel_dropdown.onclick = function(){
     dropdown.style.transform = "translateX(-150%)";
     img_container.style.opacity = "1";
     this.style.display = "none";
@@ -22,24 +26,32 @@ img_container.onclick = function(){
     dropdown.style.transform = "translateX(-150%)";
     search_form.style.display = "none";
     this.style.opacity = "1";
-    cancel.style.display = "none";
 }
 
-document.getElementById('search_icon').onclick = function(){
-    search_form.style.width = "90%";
-    search_form.style.margin = "auto";
-    search_form.style.display = "block";
+search_icon.onclick = function(){
+    search_form.style.width = "100%";
+    search_form.style.display = "inline";
     dropdown.style.display = "none";
-    this.style.fontSize = "1.5rem";
-    
+    this.style.display = "none";
+    cancel_search.style.display = "inline"; 
+    ham_icon.style.display = "inline";
+    cancel_search.style.display = "inline";
+    img_container.style.opacity = "1";
+    input_field.style.width = "80%";
 }
-document.getElementById("ham_icon").onclick = function(){
+cancel_search.onclick = function(){
+    search_form.style.display = "none";
+    search_icon.style.display = "inline";
+    ham_icon.style.display = "inline";
+}
+ham_icon.onclick = function(){
     search_form.style.display = "none";
     dropdown.style.display = "block";
     dropdown.style.transform = "translateX(0%)";   
     img_container.style.opacity = "0.7";
-    cancel.style.display = "inline";
-    this.style.display = "none";
+    cancel_search.style.display = "none"; 
+    search_icon.style.display = "inline"; 
+    cancel_dropdown.style.display = "inline";
 }
 let my_shoe_category = document.getElementsByClassName("shoe_categpry");
 for(let i = 0; i <= my_shoe_category.length; i++){
