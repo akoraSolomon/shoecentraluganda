ad47511dc30a8d24d21fa882b430bdfa987bcd75
Akora Solomon <35068415+akoraSolomon@users.noreply.github.com>
2022-03-09 11:04:13 +0300

    site 2.0

--- js/script.js
+++ js/script.js
@@ -1,106 +1,58 @@
-var input_field = document.getElementById("search_shoe");
-var search_form = document.getElementById("search_form");
-var dropdown = document.getElementById("dropdown_id");
-var search_icon = document.getElementById('search_icon');
-var cancel_search = document.getElementById('cancel_search');
-var img_container = document.getElementById("img_hero");
-var ham_icon = document.getElementById("ham_icon");
-var cancel_dropdown = document.getElementById("cancel_dropdown");
-var shoes = document.getElementsByClassName("shoes");
-var single_shoe = document.getElementById("single_shoe");
-const female_shoe_category = document.getElementsByClassName("female_shoes");
-
-let year = new Date();
-cancel_dropdown.onclick = function(){
-    img_container.style.opacity = "1";
-    ham_icon.style.display = "inline";
-}
-img_container.onclick = function(){
-    dropdown.style.transform = "translateX(-150%)";
-    search_form.style.display = "none";
-    this.style.opacity = "1";
-    search_icon.style.display = "inline"; 
-}
-
 $(document).ready(function(){
-    $(".search_shoe_wrapper").hide();
     $("section.shoe_details").hide();
     $("#cancel_dropdown").hide();
     $("#dropdown_id").hide();
-    $("#go_back").hide();
+    $("#go_back").hide();    
     function setTop(){         
         return document.documentElement.scrollTop = 0;
     }
     function setHamIconBorder(){
         return $(".menu_hamburger").css({"border":"1px solid #131c27"});
     }
-    function slideDownSearchShoeWrapper(){
-        animateSearchBarWidth();
-        fadeInCancelSearchIcon(); 
-        return $(".search_shoe_wrapper").slideDown(500);
+    function setFooterFadeTo(duration, visibility){
+        $("footer").fadeTo(duration, visibility);
     }
-    function transformCancelSearchBar(){
-        return $(".cancel_search_bar").css({"transform":"scale(0.8)", "transition":"transform 1.5s .5s"});
-    }
-    function fadeInCancelSearchIcon(){
-        return $("#cancel_search").fadeIn(1500);
-    }
-    function shoppingBagFixedPosition(){
-        return  $(".search_and_bag_icons").css({"justify-content":"right"});
-    }
-    function repositionSearchIconAndBag(){
-        return  $(".search_and_bag_icons").css({"justify-content":"space-between"});
-    }
-    function fadeOutSearchIcon(){
-        return $("#cancel_search").fadeOut(1500);
-    }
-    function fadeShoeImagesContainer(){
-        return $("#img_hero").fadeTo("fast", 0.5);
-    }
-    function animateSearchBarWidth(){  
-        searchBarFocusIn();      
-        searchBarFocusOut();
-        return $("#search_shoe").animate({width:"100%"},"slow");
+    function fadeSingleShoeTo(duration, visibility){
+        $("#single_shoe_specs").fadeTo(duration, visibility);
     }
     function searchBarFocusIn(){
-        let focus_in = $("#search_shoe").focus(function(){
-            $(".cancel_search_bar").css({"box-shadow":"none", "color":"#131c27", "border": "none","transform":"scale(1.2)","transition": "all 1s"});
-            $(this).css({"border": "1px solid #131c27"});
-        });
-        return focus_in;
+            $(".cancel_search_bar").css({"box-shadow":"none", "color":"#131c27", "background": "none", "border": "none","transform":"scale(1.2)","transition": "all 1s"});
+            $("#search_shoe").css({"border": "1px solid #131c27", "background": "white"});
     }
     function searchBarFocusOut(){
-        let focus_out = $("#search_shoe").focusout(function(){
-            $(".cancel_search_bar").css({"color": "#131c27ad","box-shadow":"0.1em 0.1em 0.2em #131c2754, -0.09em -0.09em 0.2em  #131c2754","transform":"scale(1)", "background-color": "rgba(238, 238, 238, 0.248)"});
-            $(this).css({"border": "none","background-color": "rgba(238, 238, 238, 0.248)", "box-shadow":"0.1em 0.1em 0.25em #131c2754, -0.1em -0.1em 0.25em  #131c2754","transition": "all 1s"});
-        });
-        return focus_out;
+        $(".cancel_search_bar").css({"color": "#131c27ad","box-shadow":"0.1em 0.1em 0.2em #131c2754, -0.09em -0.09em 0.2em  #131c2754","transform":"scale(1)", "background-color": "rgba(238, 238, 238, 0.248)"});
+        $("#search_shoe").css({"border": "none","background-color": "rgba(238, 238, 238, 0.248)", "box-shadow":"0.1em 0.1em 0.25em #131c2754, -0.1em -0.1em 0.25em  #131c2754","transition": "all 1s"});
+
     }
     function translateImhHeroToTwenty(){
         $("#img_hero").css({"transform":"translateX(20%)", "transition":"transform 1s"});
     }
     function restoreMainHeadZIndex(){
-        $(".main-head").fadeTo("fast", 0.5).css("z-index","0");
+        $(".main-head").fadeTo("fast", 1).css("z-index","1");
     }
 
-    function scaleLogoContainer(){
-        $(".logo_container").children("h1").css({"transform":"scale(0.25)", "transition":"transform 1s"});
+    function scaleLogo(scale){
+        $(".logo_container").children("h1").css({"transform":scale, "transition":"transform 1s"});
     }
-    function showDropdownMenu(){
-        $("#dropdown_id").show(500).css({"transform": "translateX(0%) scaleX(1)"});
+    function setFooterZIndex(value){
+        $("footer").css({"z-index": value});
     }
-    function hideDropdownMenu(){
-        $("#dropdown_id").css({"transform": "translateX(-150%) scaleX(0.25)", "transition":"transform 1s"}).hide(1000);
-    }
-    function fadeImgHeroToHalf(){
-        $("#img_hero").fadeTo("fast", 0.5);
+    function fadeImgHeroTo(half){
+        $("#img_hero").fadeTo("fast", half);
     }
 
     function displayCancelDropdown(){
         $("#cancel_dropdown").css("display","inline");
     }
+    function scaleHeart(scale){
+        $(".heart span").css({"transform":scale, "transition":"transform 1.5s"});
+    }
+    
     function setZIndexMainHead(z_index_num){
         $(".main-head").css({"z-index":z_index_num});
+    }
+    function setTranslateDropdown(){
+        $("#dropdown_id").show(500).css({"transform": "translateX(0%) scaleX(1)", "transition":"transform 1s"})
     }
     function setZIndexImgHero(z_index_num){
         $("#img_hero").css({"z-index":z_index_num});
@@ -108,64 +60,95 @@ $(document).ready(function(){
     function setZIndexDropdown_menu(){
         $("#dropdown_id").css({"z-index":"1"});
     }
-    function fadeMainHeadToHalf(){
-        $(".main-head").fadeTo("fast", 0.5);
+    function fadeMainHeadTo(duration, visibility){
+        $(".main-head").fadeTo(duration, visibility);
     }
-    function restoreImgHeroFadeToOne(){
-       return $("#img_hero").fadeTo("fast", 1);
+    function scaleCancelSearchIcon(scale, transition){
+        $(".cancel_search_bar").css({"transform":scale, "transition":transition});
     }
-    function restoreImgHeroWidth(){
-        return $("#img_hero").css({"transform":"translateX(0%)", "transition":"transform 1s"});
-     }
-    function onClickOutsideDropdown(){
-        // restoreImgHeroFadeToOne();
-        // restoreImgHeroWidth();
-        hideDropdownMenu()
-    }
+    
     $("#search_icon").click(function(){
-        setTop(); 
-        slideDownSearchShoeWrapper();
-        transformCancelSearchBar(); 
-        restoreImgHeroFadeToOne();
-        hideDropDownMenu();
-        $(this).fadeOut(1000, function(){
-            shoppingBagFixedPosition();
-        });        
+        $(".main-head").fadeOut(100, function(){
+            $(".search_shoe_wrapper").fadeIn(100, function(){
+                $("#search_shoe").animate({width:"100%"},"slow", function(){
+                    $(this).focus();
+                    searchBarFocusIn();
+                });
+                scaleCancelSearchIcon("scale(0.7)", "transform 1s" )
+                setZIndexImgHero(-1); 
+            });
+        });      
+           
     });
-    $("#cancel_search").click(function(){
-        fadeOutSearchIcon();
-        $("i#search_icon").fadeIn(500);
-        $(".cancel_search_bar").css({"transform":"scale(1)", "transition":"transform 1.5s"});
-        $(".search_shoe_wrapper").slideUp(500);
-        $("#search_shoe").animate({width:"5%"},"slow");
-        $(this).css({"transform":"scale(0.5)", "transition":"transform 1.5s"}).fadeOut(500);
-        repositionSearchIconAndBag();
-        $(this).fadeIn(500);
+    $("#shopping_bag").click(function(){
+        let empty = "Shopping Bag Is Empty!";
+        let cancel_alert = "x";
+        $("#shopping_cart_notification").addClass("shop_bag_notification_content").text(empty);
+        $("#shopping_alert_container").addClass("shop_bag_alert");
+        $("#cancel_shop_bag_alert").text(cancel_alert).addClass("cancel_shop_bag_alert");
+    });
+    $("#cancel_search").click(cancelSearchBar = function(){          
+        $("#search_shoe").animate({width:"0%"},"slow", function(){
+            $(".search_shoe_wrapper").css({"display":"none"});
+            $(".main-head").fadeIn(500, function(){
+                setZIndexImgHero(0);
+            }); 
+            $(this).focusout();
+            searchBarFocusOut();
+        });         
     });
     $("#ham_icon").click(function(){    
         setHamIconBorder();  
         setZIndexMainHead(-1);
         setZIndexDropdown_menu();
-        setZIndexImgHero(-2);   
-        fadeImgHeroToHalf();
+        setZIndexImgHero(-2); 
+        fadeImgHeroTo(0.5);
         displayCancelDropdown();
-        fadeMainHeadToHalf();
-        scaleLogoContainer();
-        showDropdownMenu();
+        fadeMainHeadTo("slow", 0.3);
+        scaleLogo("scale(0.25)")
+        setTranslateDropdown();
         translateImhHeroToTwenty();
-                   
+        setFooterFadeTo("slow", 0.3);
+        setFooterZIndex(-1);
+        if($("#single_shoe_specs").css("display") == "grid"){
+            fadeSingleShoeTo("slow", 0.3);
+        }                   
     });
-    $("#cancel_dropdown").click(cancelDropdownMenu = function(){     
+    $("#cancel_dropdown").click(function(){     
         setTop();  
         setZIndexImgHero(0); 
         setZIndexMainHead(1);
-        $(".main-head").fadeTo("fast", 1).css("z-index","0");
+        setFooterFadeTo("slow", 1);
+        setFooterZIndex(0);
+        fadeImgHeroTo(1);
+        if($("#single_shoe_specs").css("display") == "grid"){
+            fadeSingleShoeTo("slow", 1);
+        }
+        
+        $(".main-head").fadeTo("fast", 1).css("z-index","1");
+        scaleLogo("scale(1)")
         $("#logo1").css({"transform":"scale(1)", "transition":"transform 1s"});
         $("#img_hero").css({"transform":"translateX(0%)", "transition":"transform 1s"});
         $("#dropdown_id").css({"transform": "translateX(-150%) scaleX(0.25)", "transition":"transform 1s"}).hide(1000);
     });
-    $(".shoes").click(function(){
-
+    $(".shoes").click(function(){   
+        setTop();     
+        $(".main-head").fadeOut(500, function(){            
+            $("#single_shoe_specs").fadeIn(500, function(){
+                $("#go_back").fadeIn(500);
+                scaleHeart("scale(1.3)");                
+                $("main").addClass("image_slider_container");
+                $("section#img_hero").removeClass("hero").addClass("image_slider");
+                $("section.image_slider").on("touchmove", function(){
+                    if($(this).css("margin-left") == "0px"){
+                        $(this).animate({marginLeft : "-=210px"}, "slow");
+                    }else{
+                    }
+                    
+                });
+            });
+            $("#single_shoe_specs").css("display", "grid");
+        });
         var clicked_shoe_src = $(this).attr("src");
         var id_of_clicked_shoe = $(this).attr("id");
         $("#single_shoe").attr("src", clicked_shoe_src);
@@ -245,33 +228,30 @@ $(document).ready(function(){
                 $(".shoe_subtitle").text("shoe: n/a");
                 $("#buy").removeClass("add_to_bag").addClass("add_to_bag_inactive");
             }
-        }
-                      
-       $("#single_shoe_specs").css("display", "grid").show(500);
-       setTop(); 
-       $("#go_back").show(500);
-       $("main").addClass("image_slider_container");
-       $("section#img_hero").removeClass("hero").addClass("image_slider");
-       $("section.image_slider").on("touchmove", function(){
-           if($(this).css("margin-left") == "0px"){
-               $(this).animate({marginLeft : "-=210px"}, "slow");
-           }else{
-           }
-           
-       });
+        }                     
+        
     });
-    $("#go_back").click(function(){
-        $("section").filter("#img_hero").show(500);
-        $("#single_shoe_specs").hide(500);
-        $("#buy").css({"box-shadow":"none", "transition":"box-shadow 1s"});
-        $("section#img_hero").removeClass("image_slider").addClass("hero");
+    $(".heart").click(function(){
+    });
+    $("#go_back").click(function(){      
+        $("#single_shoe_specs").fadeOut(100, function(){
+            $(".main-head").fadeIn(500);
+            $("#img_hero").fadeIn(500);
+            $("#buy").css({"box-shadow":"none", "transition":"box-shadow 1s"});
+            $("#img_hero").removeClass("image_slider").addClass("hero");
+            scaleHeart("scale(0.3)");
+        });
+        
     });
     $(".shoe_categpry").click(function(){
-        $(this).css({"background-color":"#43516191",
-                    "transition":"background 0.5s cubic-bezier(.5,.8,.5,.8)"});
+        
+        $(this).css({"background-color":"#43516191", "transition":"background 0.5s cubic-bezier(.5,.8,.5,.8)"});
+        cancel_dropdown();
         restoreMainHeadZIndex();
     });
-
+    let year = new Date();
+    $("date").text(year.getFullYear());
 });
 
-document.getElementById("date").innerHTML = year.getFullYear();
+
+
